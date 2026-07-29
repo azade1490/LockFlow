0f195dcb9f746b4d0f942c17abde3fc80a165a80
Mahnaz azad <azade1490@gmail.com>
2026-07-29 06:45:54 +0330

    Update ValueObject.cs

--- MicroStore.OrderService/Domain/Common/ValueObject.cs
+++ MicroStore.OrderService/Domain/Common/ValueObject.cs
@@ -1,13 +1,4 @@
 ﻿namespace MicroStore.OrderService.Domain.Common;
-// record انتخاب بهتری است، چون ویژگی‌های اصلی Value Object را به‌صورت پیش‌فرض دارد:
-// مقایسه بر اساس مقدار (Value Equality)
-// پیاده‌سازی Equals
-// پیاده‌سازی GetHashCode
-// عملگرهای == و !=
-// پشتیبانی مناسب از Immutable بودن
-// اما چند استثنا وجود دارد:
-// اگر می‌خواهید تمام Value Objectها متدهای مشترکی داشته باشند (مثلاً Validate() یا Clone()).
-// اگر می‌خواهید رفتار مقایسه را به شکلی متفاوت از رفتار پیش‌فرض record پیاده‌سازی کنید.
 public abstract class ValueObject
 {
     protected abstract IEnumerable<object?> GetEqualityComponents();
