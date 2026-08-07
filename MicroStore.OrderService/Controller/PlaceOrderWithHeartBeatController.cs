bc10a9a1881e6cd8fd0b2bda0b6b418d0078a7fc
Mahnaz azad <azade1490@gmail.com>
2026-08-07 03:42:06 +0000

    Update PlaceOrderWithHeartBeatController.cs

--- MicroStore.OrderService/Controller/PlaceOrderWithHeartBeatController.cs
+++ MicroStore.OrderService/Controller/PlaceOrderWithHeartBeatController.cs
@@ -111,18 +111,12 @@ public class PlaceOrderWithHeartBeatController : ControllerBase
                 stockKey,
                 currentStock);
 
-            // اطلاعات سفارش را تکمیل می‌کنیم  
             orderDto.ID = Guid.NewGuid();
-
             Address address = new Address("Iran", "Tehran", "Tehran", "street", "123456789");
-
-            // ساخت موجودیت سفارش  
             var order = new MicroStore.OrderService.Domain.Order.AggregateRoot.Order(orderDto.ID, address);
-
             Money money = new Money(1000000000, "IRR");
             order.AddItem(Guid.NewGuid(), "LapTop", money, 2);
 
-            // ذخیره سفارش در پایگاه داده  
             _context.Orders.Add(order);
             await _context.SaveChangesAsync();
 
