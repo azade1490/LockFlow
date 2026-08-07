749fd930cab99c43923ee5a11032f89f430378c4
Mahnaz azad <azade1490@gmail.com>
2026-08-07 23:36:19 +0000

    Update OrderDto.cs

--- MicroStore.OrderService/DTO/OrderDto.cs
+++ MicroStore.OrderService/DTO/OrderDto.cs
@@ -1,8 +1,6 @@
 ﻿namespace MicroStore.OrderService.DTO;
 public class OrderDto
 {
-    public Guid ID { get; set; }
-
     public int ProductId { get; set; }
 
     public int Quantity { get; set; }
