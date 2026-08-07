39c6b1b3f12d2a86617a98b253385b766761bca2
Mahnaz azad <azade1490@gmail.com>
2026-08-07 04:32:40 +0000

    Update OrderQueueWorkerWithHeartBeat.cs

--- MicroStore.OrderService/BackgroundWorkers/OrderQueueWorkerWithHeartBeat.cs
+++ MicroStore.OrderService/BackgroundWorkers/OrderQueueWorkerWithHeartBeat.cs
@@ -126,7 +126,7 @@ public sealed class OrderQueueWorkerWithHeartBeat : BackgroundService
             Address address = new Address("Iran", "Tehran", "Tehran", "street", "123456789");
             var order = new MicroStore.OrderService.Domain.Order.AggregateRoot.Order(orderDto.ID, address);
             var money = new Money(1000000000, "IRR");
-            order.AddItem(Guid.NewGuid(), "LapTop", money, 2);
+            order.AddItem(Guid.NewGuid(), "LapTop", money, orderDto.Quantity);
 
             context.Orders.Add(order);
 
