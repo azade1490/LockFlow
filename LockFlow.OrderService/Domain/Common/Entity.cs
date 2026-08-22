4f1e35ca21f3e0ededbf7e515fa84f3c0ab3cc28
Mahnaz <azade1490@gmail.com>
2026-08-22 07:32:06 +0330

    Edit Entity

--- LockFlow.OrderService/Domain/Common/Entity.cs
+++ LockFlow.OrderService/Domain/Common/Entity.cs
@@ -5,7 +5,13 @@ public abstract class Entity<TId>
 
     public override bool Equals(object? obj)
     {
+        if (ReferenceEquals(this, obj))
+            return true;
+
         if (obj is not Entity<TId> other)
+            return false;
+
+        if (GetType() != other.GetType())
             return false;
 
         return EqualityComparer<TId>.Default.Equals(Id, other.Id);
